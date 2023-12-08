0b2aabaf30d449fae8adc6413fea683dc9ae38af
DavitPetrosyan12 <davitpet1@gmail.com>
2023-12-08 16:54:40 +0400

    commit

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,6 +1,8 @@
 import PageBanner from "../common/page-banner/PageBanner";
 import ValidationSchemaExample from "./formik/Formik";
+
 import "./Feedback.scss"
+
 const Feedback = () => {
   
   return <div className="feedback">
